80dbd7174bbae6faf59f5dd24f3d830c7f9754c4
Mike Thomas <mthomas@tokbox.com>
2017-03-17 11:14:35 +0100

    update travis and build scripts for new dir structure

--- build.sh
+++ build.sh
@@ -6,19 +6,12 @@ force="$2"
 
 #Create zip file with JS and doc
 if [ "$task" == "-d" ]; then
- if [[ -d opentok.js-annotations ]]
- then
-	 cd opentok.js-annotations
 	 npm i
 	 npm update
 	 gulp dist
 	 cd dist
 	 gulp zip
 	 exit 0
- else
-	 echo "Please run this script from 'JS'."
-	 exit 1
- fi
 fi
 
 if [ "$task" == "-t" ]; then
