1648ef59d43f7e8ffcf6738ca1acc442280caa86
Mike Thomas <mthomas@tokbox.com>
2017-03-23 16:30:37 +0100

    Replace gulp and build with npm scripts

--- build.sh
+++ build.sh
@@ -4,21 +4,11 @@ set -e
 task="$1"
 force="$2"
 
-#Create zip file with JS and doc
-if [ "$task" == "-d" ]; then
-	 npm i
-	 npm update
-	 gulp dist
-	 cd dist
-	 gulp zip
-	 exit 0
-fi
-
 if [ "$task" == "-t" ]; then
 	cd opentok.js-annotations
 	grunt karma:prod "$force"
 	exit 0
 fi
 
-echo Invalid parameters, please use ‘-t’ to run tests or ‘-d’ to create zip file with JS and doc.
+echo Invalid parameters, please use ‘-t’ to run tests.
 exit 1
