ec005c164797b7fe8359a6fd9dfd8b6a2963e098
acn <acn@alberto.local>
2017-03-31 16:52:04 +0200

    Adding extra config in karma file for saucelabs

--- karma.conf.js
+++ karma.conf.js
@@ -54,8 +54,14 @@ module.exports = function(config) {
         },
         sauceLabs: {
           testName: 'Accelerator Annotation Unit Tests',
-          username: process.env.SAUCE_USERNAME,
-          accessKey: process.env.SAUCE_ACCESS_KEY
+          tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+            username: process.env.SAUCE_USERNAME,
+            accessKey: process.env.SAUCE_ACCESS_KEY,
+            startConnect: false,
+            connectOptions: {
+                port: 5757,
+                logfile: 'sauce_connect.log'
+            }
         }
     };
 
